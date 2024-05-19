9e6e4106085a370c4cb4711d1c6cf267ff29a03a
Usmon <usmonpahlavonov@gmail.com>
2024-05-19 18:36:46 +0900

    init backend

---
+++ backend/squidbuilder/src/main/java/com/boyz/squidbuilder/SquidbuilderApplication.java
@@ -0,0 +1,13 @@
+package com.boyz.squidbuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SquidbuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SquidbuilderApplication.class, args);
+	}
+
+}
---
+++ backend/squidbuilder/src/test/java/com/boyz/squidbuilder/SquidbuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boyz.squidbuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SquidbuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
