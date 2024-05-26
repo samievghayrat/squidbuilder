548516abd850e099e3d60bff7fc64793d7ab2415
Usmon <usmonpahlavonov@gmail.com>
2024-05-26 16:56:56 +0900

    added Login/Register pages; connected to backend;

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginForm from './components/LoginForm';
+import RegisterForm from './components/RegisterForm';
+import './index';
 
-function App() {
+
+const App = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const loggedInStatus = localStorage.getItem('isLoggedIn') === 'true';
+    setIsLoggedIn(loggedInStatus);
+  }, []);
+
+  const handleLogin = (data) => {
+    console.log("Login data:", data);
+    setIsLoggedIn(true);
+    localStorage.setItem('isLoggedIn', 'true');
+  };
+
+  const handleRegister = (data) => {
+    console.log("Register data:", data);
+    setIsLoggedIn(true);
+    localStorage.setItem('isLoggedIn', 'true');
+  };
+
+  const handleLogout = () => {
+    setIsLoggedIn(false);
+    localStorage.removeItem('isLoggedIn');
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={ 
+            <div className="App">
+            <h1>Welcome Bratishka!!</h1>
+            {isLogin ? (
+              <LoginForm onLogin={handleLogin} />
+            ) : (
+              <RegisterForm onRegister={handleRegister} />
+            )}
+            <button className="toggle-button" onClick={() => setIsLogin(!isLogin)}>
+              {isLogin ? "Don't have an account? Register" : "Already have an account? Login"}
+            </button>
+          </div>
+        } />
+      </Routes>
+    </Router>
   );
-}
+};
 
 export default App;
